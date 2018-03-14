b726d68b20ed59ec9b1839aef34f8ded85907633
Jasper <mr.jasperfriend@gmail.com>
2018-03-14 17:01:48 +0000

    Work 14-03-18

--- ERP/View/AccountView.xaml.cs
+++ ERP/View/AccountView.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using MaterialDesignThemes.Wpf;
+using ERP.Model;
+using System.Collections.ObjectModel;
 
 namespace ERP.View
 {
@@ -25,6 +27,78 @@ namespace ERP.View
         {
             Debug.Print(accountID.ToString());
             InitializeComponent();
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+            _accountList.Add(new Account() { accountCode = "TIMC", name = "Tim Co.", email = "tim@tim.co", telephone = "0800 000 001" });
+            _accountList.Add(new Account() { accountCode = "BOBC", name = "Bob Co.", email = "bob@bob.co", telephone = "0800 000 002" });
+            _accountList.Add(new Account() { accountCode = "ROBC", name = "Rob Co.", email = "rob@rob.co", telephone = "0800 000 003" });
+            _accountList.Add(new Account() { accountCode = "JIMC", name = "Jim Co.", email = "jim@jim.co", telephone = "0800 000 004" });
+            _accountList.Add(new Account() { accountCode = "SAMC", name = "Sam Co.", email = "sam@sam.co", telephone = "0800 000 005" });
+        }
+
+        private Account _selectedItem;
+        public Account selectedItem
+        {
+            get { return _selectedItem; }
+            set { if (value != null) { _selectedItem = value; } }
+        }
+
+        private ObservableCollection<Account> _accountList;
+        public ObservableCollection<Account> accountList
+        {
+            get { return _accountList ?? (_accountList = new ObservableCollection<Account>()); }
+        }
+
+        public int recordCount
+        {
+            get { return accountList.Count(); }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            string theValue = button.Tag.ToString();
+            Debug.Print(theValue);
+            var newWindow = new AccountView(1);
+            newWindow.Show();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
--- ERP/View/LoginView.xaml.cs
+++ ERP/View/LoginView.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -17,8 +19,20 @@ namespace ERP.View
     /// <summary>
     /// Interaction logic for LoginView.xaml
     /// </summary>
-    public partial class LoginView : Window
+    public partial class LoginView : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
         public LoginView()
         {
             InitializeComponent();
@@ -26,11 +40,36 @@ namespace ERP.View
 
         private void SignIn_Click(object sender, RoutedEventArgs e)
         {
+            if (username != null)
+            {
+                Properties.Settings.Default["Username"] = username;
+                Properties.Settings.Default.Save();
+            }
+            loginTimer = new DispatcherTimer();
+            loginTimer.Tick += loginTimer_Tick;
+            loginTimer.Interval = new TimeSpan(0, 0, 5);
+            loginTimer.Start();
+            LoginProgress.Visibility = Visibility.Visible;
+        }
+
+        private DispatcherTimer loginTimer;
+
+        private void loginTimer_Tick(object sender, EventArgs e)
+        {
             Dispatcher.BeginInvoke(new Action(() => {
+                loginTimer.Stop();
                 var editwindow = new MainWindow();
                 editwindow.Show();
+                editwindow.Focus();
                 this.Close();
             }));
+        }
+
+        private string _username;
+        public string username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged("username"); }
         }
     }
 }
