b4b6939a4d205aea874dc1b5d097d16501417288
Jasprrr <mr.jasperfriend@gmail.com>
2018-03-15 22:25:30 +0000

    home 15-03-18

---
+++ ERP/Model/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Model
+{
+    class Contact
+    {
+        public int contactID { get; set; }
+        public string forename { get; set; }
+        public string surname { get; set; }
+        public string telephone { get; set; }
+        public string mobile { get; set; }
+        public string primaryEmail { get; set; }
+        public string secondaryEmail { get; set; }
+        public string accountID { get; set; }
+    }
+}
